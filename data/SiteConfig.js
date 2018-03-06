7bdedf09393a5ff38173876c4f6772d06d4d6c0e
Bryce Fischer <bryce-fischer@idexx.com>
2018-03-06 15:27:04 -0500

    added prefix

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,4 +1,5 @@
 module.exports = {
   siteTitle: 'BZ Guitar Library', // Site title.
   siteTitleAlt: 'Library of guitar tabs, riffs, exercises, etc', // Alternative site title for SEO.
+  pathPrefix: 'bz-guitar-lib',
 };
